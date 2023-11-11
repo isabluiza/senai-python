3b6c824c794840dc7772e740b87c4f1086766d42
Isabella Luiza Souza <79116590+isabluiza@users.noreply.github.com>
2023-11-11 10:46:33 -0300

    Update 037.py

--- resolucoes/037.py
+++ resolucoes/037.py
@@ -1,6 +1,5 @@
-# Escreva um programa que peça ao usuário para adivinhar um número entre 1 e 10
-# e continue pedindo até que o usuário acerte o número.
-# E no final, retorne também a quantidade de tentativas necessárias.
+# Escreva um programa que peça ao usuário para adivinhar um número entre 1 e 10 e continue pedindo até que o usuário acerte o número.
+# No final, retorne também a quantidade de tentativas necessárias.
 
 import random
 
