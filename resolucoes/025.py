2765bc03de46f2b5b5a199083aee3a27e86f679d
Isabella Luiza Souza <79116590+isabluiza@users.noreply.github.com>
2023-11-11 10:44:17 -0300

    Update 025.py

--- resolucoes/025.py
+++ resolucoes/025.py
@@ -1,45 +1,50 @@
 # Crie um programa para jogar JOKEMPO, usando a função random.randint
+
 import random
 
-Escolha_jogador = int(input('\n Digite uma das seguintes opções: '
+jogador = int(input('\n Digite uma das seguintes opções: '
                     '\n1 - Pedra'
                     '\n2 - Papel'
-                    '\n3 - Tesoura'
-                    '\n Qual a sua escolha? '))
+                    '\n3 - Tesoura\n'))
 
-Escolha_PC = random.randint(1,3)
+maquina = random.randint(1,3)
 
-if Escolha_jogador > 3 or Escolha_jogador < 1:
+if jogador > 3 or jogador < 1:
     print('Escolha inválida')
+
 else:
-    if Escolha_PC == Escolha_jogador:
-        print('Empatamos - Foi por Pouco')
+    if maquina == jogador:
+        print('Empatamos. Ninguém ganhou')
 
-    elif Escolha_PC == 1 and Escolha_jogador == 3:
+    elif maquina == 1 and jogador == 3:
         print('Pedra x Tesoura'
-              '\nEu Ganhei')
+              '\nEu ganhei!')
 
-    elif Escolha_PC == 2 and Escolha_jogador == 1:
+    elif maquina == 2 and jogador == 1:
         print('Papel x Pedra'
-              '\nEu Ganhei')
+              '\nEu ganhei!')
 
-    elif Escolha_PC == 3 and Escolha_jogador == 2:
+    elif maquina == 3 and jogador == 2:
         print('Tesoura x Papel'
-              '\nEu Ganhei')
+              '\nEu ganhei!')
 
     else:
-        if Escolha_PC == 1:
-            Escolha_PC = 'Pedra'
-        elif Escolha_PC == 2:
-            Escolha_PC = 'Papel'
-        else:
-            Escolha_PC = 'Tesoura'
+        if maquina == 1:
+            maquina = 'Pedra'
 
-        if Escolha_jogador == 1:
-            Escolha_jogador = 'Pedra'
-        elif Escolha_jogador == 2:
-            Escolha_jogador = 'Papel'
-        else:
-            Escolha_jogador = 'Tesoura'
+        elif maquina == 2:
+            maquina = 'Papel'
 
-        print(f'Escolha PC: {Escolha_PC} X {Escolha_jogador}, Você Ganhou')
+        else:
+            maquina = 'Tesoura'
+
+        if jogador == 1:
+            jogador = 'Pedra'
+
+        elif jogador == 2:
+            jogador = 'Papel'
+
+        else:
+            jogador = 'Tesoura'
+
+        print(f'{maquina} X {jogador}. Você ganhou!')
