a3b69087b12f20068c48f3a7fc52a08b84f179ee
Isabella Luiza Souza <79116590+isabluiza@users.noreply.github.com>
2023-11-11 10:47:33 -0300

    Update 040.py

--- resolucoes/040.py
+++ resolucoes/040.py
@@ -1,5 +1,4 @@
-# Escreva um programa que leia um número n inteiro qualquer
-# e mostra na tela os n primeiros elementos de uma Sequência de Fibonacci
+# Escreva um programa que leia um número n inteiro qualquer e mostra na tela os n primeiros elementos de uma Sequência de Fibonacci
 # 1 - 1 - 2 - 3 - 5 - 8 - 13 - 21
 #   0 - 1 - 1 - 2 - 3 - 5 - 8
 
